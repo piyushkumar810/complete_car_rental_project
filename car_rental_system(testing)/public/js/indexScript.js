ebe855a62ca5633a1ca1ba8868b0cf7c5bf0417d
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-05-08 23:16:15 +0530

    Car Rental Project

---
+++ car_rental_system(testing)/public/js/indexScript.js
@@ -0,0 +1,23 @@
+// let search=document.querySelector('.search-box');
+// document.querySelector('#search-icon').onclick=()=>{
+//     search.classList.toggle('active')
+// }
+
+let menu=document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick=()=>{
+    menu.classList.toggle('active')
+}
+
+
+// hide menu and Search box on scroll
+window.onscroll=()=>{
+    menu.classList.remove('active')
+}
+
+// ------------------header------------------
+
+let header=document.querySelector('header');
+
+window.addEventListener('scroll',()=>{
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
