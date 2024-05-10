65ed6241a38b6b95ddc3e55a139a48a2bbb11778
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-05-10 18:23:30 +0530

    added back button

--- car_rental_system(testing)/public/js/loginScript.js
+++ car_rental_system(testing)/public/js/loginScript.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('.container');
 const  loginLink= document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+// const closeIcon=document.querySelector('.icon-close')
+
+
 
 registerLink.addEventListener('click', ()=>{
     container.classList.add('active')
@@ -9,3 +12,9 @@ registerLink.addEventListener('click', ()=>{
 loginLink.addEventListener('click', ()=>{
     container.classList.remove('active')
 })
+
+// applying action on cross icon
+// closeIcon.addEventListener('click', ()=>{
+//     container.classList.remove('active')
+// })
+
