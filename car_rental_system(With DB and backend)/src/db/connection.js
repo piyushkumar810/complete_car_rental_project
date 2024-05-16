0d38982bcd724cecbf6c968d1d8b7b4152e2ff1b
Surya <suryask7549@gmail.com>
2024-05-16 23:37:23 +0530

    Files Added

--- car_rental_system(With DB and backend)/src/db/connection.js
+++ car_rental_system(With DB and backend)/src/db/connection.js
@@ -7,37 +7,6 @@ const mongoose = require("mongoose");
   } catch (error) {
     console.log(" Failed to connect ", error);
   }
-  const UsersSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true
-    },
-    contact: {
-      type: Number,
-      required: true,
-      length: 10
-    },
-    email: {
-      type: email,
-      required: true,
-      unique: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    ulternateMOB: {
-      type: Number,
-      length: 10
-    },
-    ulternateEmail: {
-      type: email,
-      unique: true
-    },
-  })
 
   // Applying Hashing  
-
-
-  const UsersModel = new mongoose.model("UsersModel", UsersSchema);
 })();
